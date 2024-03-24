1db048dfd9ccdb1c2a34803dab4a992290bca8da
jia5232 <jia5232@naver.com>
2024-03-24 13:20:17 +0900

    feat: cors 설정 변경

--- src/main/java/com/backend/kiri/domain/Member.java
+++ src/main/java/com/backend/kiri/domain/Member.java
@@ -3,8 +3,6 @@ package com.backend.kiri.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import java.util.ArrayList;
 import java.util.List;
