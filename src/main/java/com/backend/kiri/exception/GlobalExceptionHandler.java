748381a8e11abbfe7579b0d399e7e731f2270bc2
Jia Kwon <jia5232@kookmin.ac.kr>
2024-07-01 09:44:54 +0700

    feat: 적절한 예외를 던지도록 수정

--- src/main/java/com/backend/kiri/exception/GlobalExceptionHandler.java
+++ src/main/java/com/backend/kiri/exception/GlobalExceptionHandler.java
@@ -3,110 +3,107 @@ package com.backend.kiri.exception;
 import com.backend.kiri.exception.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ErrorResult exHandle(Exception e) {
+    public ResponseEntity<ErrorResult> exHandle(Exception e) {
         ErrorResult errorResult = new ErrorResult("EX", "알 수 없는 오류가 발생했습니다.");
-        return errorResult;
+        return new ResponseEntity<>(errorResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+    public ResponseEntity<ErrorResult> illegalExHandle(IllegalArgumentException e) {
         ErrorResult errorResult = new ErrorResult("BAD", "잘못된 요청입니다.");
-        return errorResult;
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ErrorResult handleNoHandlerFoundException(NoHandlerFoundException e) {
+    public ResponseEntity<ErrorResult> handleNoHandlerFoundException(NoHandlerFoundException e) {
         ErrorResult errorResult = new ErrorResult("NOT_FOUND", "요청한 페이지를 찾을 수 없습니다.");
-        return errorResult;
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotEmptyPostException.class)
-    public ErrorResult notEmptyPostExHandle(NotEmptyPostException e) {
-        ErrorResult errorResult = new ErrorResult("NOT EMPTY POST EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notEmptyPostExHandle(NotEmptyPostException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_EMPTY_POST_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ChatRoomFullException.class)
-    public ErrorResult chatRoomFullExHandle(ChatRoomFullException e) {
-        ErrorResult errorResult = new ErrorResult("CHATROOM FULL EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> chatRoomFullExHandle(ChatRoomFullException e) {
+        ErrorResult errorResult = new ErrorResult("CHATROOM_FULL_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundChatRoomException.class)
-    public ErrorResult notFoundChatRoomExHandle(NotFoundChatRoomException e) {
-        ErrorResult errorResult = new ErrorResult("NOT FOUND CHATROOM EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notFoundChatRoomExHandle(NotFoundChatRoomException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND_CHATROOM_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundMemberException.class)
-    public ErrorResult notFoundMemberExHandle(NotFoundMemberException e) {
-        ErrorResult errorResult = new ErrorResult("NOT FOUND MEMBER EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notFoundMemberExHandle(NotFoundMemberException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND_MEMBER_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundPostException.class)
-    public ErrorResult notFoundPostExHandle(NotFoundPostException e) {
-        ErrorResult errorResult = new ErrorResult("NOT FOUND POST EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notFoundPostExHandle(NotFoundPostException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND_POST_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundRefreshTokenException.class)
-    public ErrorResult notFoundRefreshTokenExHandle(NotFoundRefreshTokenException e) {
-        ErrorResult errorResult = new ErrorResult("NOT FOUND TOKEN EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notFoundRefreshTokenExHandle(NotFoundRefreshTokenException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND_TOKEN_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NotFoundUniversityException.class)
-    public ErrorResult notFoundUniversityExHandle(NotFoundUniversityException e) {
-        ErrorResult errorResult = new ErrorResult("NOT FOUND UNIVERSITY EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notFoundUniversityExHandle(NotFoundUniversityException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_FOUND_UNIVERSITY_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public ErrorResult unauthorizedAccessExHandle(UnauthorizedAccessException e) {
-        ErrorResult errorResult = new ErrorResult("UNAUTHORIZED ACCESS EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> unauthorizedAccessExHandle(UnauthorizedAccessException e) {
+        ErrorResult errorResult = new ErrorResult("UNAUTHORIZED_ACCESS_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(AlreadyExistMemberException.class)
-    public ErrorResult alreadyExistMemberExHandle(AlreadyExistMemberException e) {
-        ErrorResult errorResult = new ErrorResult("ALREADY EXIST MEMBER EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> alreadyExistMemberExHandle(AlreadyExistMemberException e) {
+        ErrorResult errorResult = new ErrorResult("ALREADY_EXIST_MEMBER_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(NotEnoughInfoException.class)
-    public ErrorResult notEnoughInfoExHandle(NotEnoughInfoException e) {
-        ErrorResult errorResult = new ErrorResult("NOT ENOUGH INFO EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> notEnoughInfoExHandle(NotEnoughInfoException e) {
+        ErrorResult errorResult = new ErrorResult("NOT_ENOUGH_INFO_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidAuthNumberException.class)
-    public ErrorResult invalidAuthNumberExHandle(InvalidAuthNumberException e) {
-        ErrorResult errorResult = new ErrorResult("INVALID AUTH NUMBER EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> invalidAuthNumberExHandle(InvalidAuthNumberException e) {
+        ErrorResult errorResult = new ErrorResult("INVALID_AUTH_NUMBER_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IncorrectPasswordException.class)
-    public ErrorResult incorrectPasswordExHandle(IncorrectPasswordException e) {
-        ErrorResult errorResult = new ErrorResult("INCORRECT PASSWORD EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> incorrectPasswordExHandle(IncorrectPasswordException e) {
+        ErrorResult errorResult = new ErrorResult("INCORRECT_PASSWORD_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(AlreadyBlockedException.class)
-    public ErrorResult alreadyBlockedExHandle(AlreadyBlockedException e) {
-        ErrorResult errorResult = new ErrorResult("ALREADY BLOCKED EX", e.getMessage());
-        return errorResult;
+    public ResponseEntity<ErrorResult> alreadyBlockedExHandle(AlreadyBlockedException e) {
+        ErrorResult errorResult = new ErrorResult("ALREADY_BLOCKED_EX", e.getMessage());
+        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
     }
 }
